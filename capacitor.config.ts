5a1f94ae4eaa3d17c2435aa4adf5e81b07605164
Heonns2023+ <nsmorenog@heon.com.co>
2023-08-17 14:45:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppIonic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
